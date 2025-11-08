ef73cced7aeef3c76f84fc5d90b4a9e6a3c2ef1a
tokurah ibrahim <tokurah10@gmail.com>
2025-11-08 13:14:19 +0100

    d

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -21,20 +21,20 @@ def is_librarian(user):
 def is_member(user):
     return user.is_authenticated and user.userprofile.role == 'Member'
 
-# Views
+# Admin view
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
+# Librarian view
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
+# Member view
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
-
-
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
