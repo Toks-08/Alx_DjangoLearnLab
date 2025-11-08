9f2391e55143a467ab2bd2ce4355ff584bba0990
tokurah ibrahim <tokurah10@gmail.com>
2025-11-08 12:32:20 +0100

    s

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library  # <-- Make sure Library is imported
+from .models import Library
+from .models import Book
 
 # Function-based view for listing all books
 def list_books(request):
