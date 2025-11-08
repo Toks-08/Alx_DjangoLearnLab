9012f58f02c392266f6d2d8db2eca99a151a0729
tokurah ibrahim <tokurah10@gmail.com>
2025-11-08 12:43:23 +0100

    d

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,18 +5,19 @@ from .models import Book
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import login
 
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, "Account created successfully. You can now log in.")
-            return redirect('login')
+            user = form.save()        # Save the new user
+            login(request, user)      # <-- Log in the user immediately
+            messages.success(request, "Account created successfully.")
+            return redirect('list_books')
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
-
 # Function-based view for listing all books
 def list_books(request):
     books = Book.objects.all()
