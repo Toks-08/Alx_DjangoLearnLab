b87a6c26dd0c7f1bb516dda4f3a0f948c239a182
tokurah ibrahim <tokurah10@gmail.com>
2025-11-08 12:48:56 +0100

    d

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,19 +1,12 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),                  # FBV
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    # Authentication URLs
-    path('accounts/login/', auth_views.LoginView.as_view(
-        template_name='relationship_app/login.html'
-    ), name='login'),
-
-    path('accounts/logout/', auth_views.LogoutView.as_view(
-        next_page='login'  # redirect to login after logout
-    ), name='logout'),
-
+    path('accounts/login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('accounts/logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('accounts/register/', views.register, name='register'),
-]
+]# must be views.register
