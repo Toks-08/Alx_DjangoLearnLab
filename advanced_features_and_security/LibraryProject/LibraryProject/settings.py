ee1a1b4393b78bb878187d51a63702694fa4a8e0
tokurah ibrahim <tokurah10@gmail.com>
2025-11-13 14:51:10 +0100

    dd

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -126,5 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/books/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
-AUTH_USER_MODEL = 'relationship_app.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
