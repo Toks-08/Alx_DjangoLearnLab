e3e81cbfc18400048b52ee9bd5ac670b069d12bb
Tokurah Ibrahim <Tokurah10@gmail.com>
2025-11-27 17:35:12 +0100

    advanced api

---
+++ advanced-api-project /advanced-api-project/api/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+# Author Model
+# Purpose: Represents a writer or contributor.
+# It holds the biographical details for each author.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+#Book Model
+# Purpose: Represents a single published work.
+# It links back to the Author and includes details about the publication.
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
---
+++ advanced-api-project /advanced-api-project/api/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import Author, Book
+from rest_framework.exceptions import ValidationError
+from datetime import date
+
+#BookSerializer
+# Purpose: Converts complex Book model instances into native Python datatypes
+# (and then JSON) for the API response. Also handles data deserialization
+# and validation for Book objects on creation/update.
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('id', 'title', 'publication_year', 'authors')
+
+        def validate_publication_year(self, value):
+            current_year = date.today().year
+            if value > current_year:
+                # Raise a ValidationError if the year is in the future
+                raise ValidationError(
+                    f"Publication year cannot be in the future. The latest valid year is {current_year}.")
+            return value
+
+#AuthorSerializer
+# Purpose: Serializes Author model instances. It includes a nested
+# representation of all related Book objects, providing a single,
+# comprehensive view of an author and their works.
+
+class AuthorSerializer(serializers.ModelSerializer):
+    book = BookSerializer(many=True, read_only=True)
+    class Meta:
+        models = Author
+        fields = ['id','name', 'book']
