d27922ca3617e6c3f7d3bb20a9cbf1acbb04bed4
Tokurah Ibrahim <tokurah10@gmail.com>
2025-12-10 22:19:36 +0100

    changed bio from a charfield to a textfield

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
-    bio = models.CharField(max_length=200)
+    bio = models.TextField()
     profile_picture = models.ImageField()
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
 
