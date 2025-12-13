0815bda2a7afed2fff7b53b833e33e72870f5d9a
tokurah ibrahim <tokurah10@gmail.com>
2025-12-13 14:06:19 +0100

    fixed feed querylogic

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,6 +3,7 @@ from .serializers import PostSerializer, CommentSerializer
 from .models import Post, Comment
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 from .permissions import IsAuthorOrReadOnly
 from rest_framework.generics import ListAPIView
 # Create your views here
@@ -32,16 +33,17 @@ class CommentView(viewsets.ModelViewSet):
 
 class FeedListView(ListAPIView):
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
-    def get_query(self):
+    def get_queryset(self):
         #1. get the list of users the current user is following
         #2. request.following.all()returns a queryset of user objects
         following_user = self.request.user.following.all()
         #3. filter posts to include only those where the author is in the following_users set
         #4. this uses the '__in' lookup
-        queryset = Post.objects.filter(author__in=following_user)
+        queryset = Post.objects.filter(author__in=following_user).order_by('-created_at')
         #5. order the post by creation date, most recent first
-        return queryset.order_by('-created_at')
+        return queryset
+
 
 
