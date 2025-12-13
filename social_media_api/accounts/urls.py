624339a3f853b340efd0060c290d998035586a22
tokurah ibrahim <tokurah10@gmail.com>
2025-12-13 14:09:32 +0100

    worked on a typo in account.urls

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('login/', LoginView.as_view()),
     path('profile/', ProfileView.as_view(), name= 'profile-view'),
     path('profile/update/', UpdateProfileView.as_view(), name= 'profile-update'),
-    path('follow/<int:user_id>',FollowUserView.as_view(), name = 'follow-user' ),
-    path('unfollow/<int:user_id>',UnfollowUserView.as_view(), name = 'unfollow-user' )
+    path('follow/<int:user_id>/',FollowUserView.as_view(), name = 'follow-user' ),
+    path('unfollow/<int:user_id>/',UnfollowUserView.as_view(), name = 'unfollow-user' )
 
 
 ]
